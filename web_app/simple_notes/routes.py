9a5f425eafea6dc6bb55c9c4e5fb31699d6cbe21
Talesard <vetero4ekcs@mail.ru>
2021-07-18 17:52:35 +0300

    style

--- web_app/simple_notes/routes.py
+++ web_app/simple_notes/routes.py
@@ -1,15 +1,48 @@
+import re
 from datetime import datetime
 
 from flask import request, redirect, url_for, render_template
 from flask_login import login_user, login_required, logout_user, current_user
+from jinja2 import evalcontextfilter, Markup
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from simple_notes import app, db, login_manager
 from simple_notes.models import Users, Notes
 
 
+@app.template_filter()
+@evalcontextfilter
+def linebreaks(_eval_ctx, value):
+    # https://gist.github.com/cemk/1324543
+    """Converts newlines into <p> and <br />s."""
+    value = re.sub(r'\r\n|\r|\n', '\n', value)  # normalize newlines
+    paras = re.split('\n{2,}', value)
+    paras = [u'<p>%s</p>' % p.replace('\n', '<br />') for p in paras]
+    paras = u'\n\n'.join(paras)
+    return Markup(paras)
+
+
+@app.template_filter()
+@evalcontextfilter
+def linebreaksbr(_eval_ctx, value):
+    # https://gist.github.com/cemk/1324543
+    """Converts newlines into <p> and <br />s."""
+    value = re.sub(r'\r\n|\r|\n', '\n', value)  # normalize newlines
+    paras = re.split('\n{2,}', value)
+    paras = [u'%s' % p.replace('\n', '<br/>') for p in paras]
+    paras = u'\n\n'.join(paras)
+    return Markup(paras)
+
+
+@login_manager.unauthorized_handler
+def unauthorized_callback():
+    # if unauthorized then redirect to login
+    return redirect(url_for('login'))
+
+
 @login_manager.user_loader
 def load_user(user_id):
+    # load user by id from database
     return Users.query.filter_by(id=user_id).first()
 
 
@@ -47,11 +80,11 @@ def login():
         password = request.form['password']
 
         user = Users.query.filter_by(username=username).first()
-        if check_password_hash(user.password, password):
+        if user and check_password_hash(user.password, password):
             login_user(user)
-            return redirect(url_for('index'))
+            return redirect(url_for('notes'))
         else:
-            # тут нужно flash
+            print('pizda')
             pass
     return render_template('login.html')
 
@@ -60,10 +93,10 @@ def login():
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('index'))
+    return redirect(url_for('notes'))
 
 
-@app.route('/', methods=['GET'])
+@app.route('/home', methods=['GET'])
 def index():
     return render_template('index.html')
 
@@ -74,7 +107,7 @@ def secret():
     return render_template('secret.html')
 
 
-@app.route('/notes', methods=['GET'])
+@app.route('/', methods=['GET'])
 @login_required
 def notes():
     curr_user_id = Users.query.filter_by(id=current_user.id).first().id
@@ -114,18 +147,16 @@ def delete_note(note_id):
     return redirect(url_for('notes'))
 
 
-@app.route('/edit_note/<note_id>', methods=['POST', 'GET'])
+@app.route('/edit_note/<note_id>', methods=['POST'])
 @login_required
 def edit_note(note_id):
-    if request.method == 'POST':
-        title = request.form['title']
-        details = request.form['details']
-        curr_user_id = Users.query.filter_by(id=current_user.id).first().id
-        if title != '':
-            Notes.query.filter_by(user_id=curr_user_id, id=note_id).update({'title': title, 'detail_text': details, 'date_upd': datetime.utcnow()})
-            db.session.flush()
-            db.session.commit()
-            return redirect(url_for('note_detail', note_id=note_id))
+    title = request.form['title']
+    details = request.form['details']
     curr_user_id = Users.query.filter_by(id=current_user.id).first().id
-    note = Notes.query.filter_by(user_id=curr_user_id, id=note_id).first()
-    return render_template('edit.html', note=note)
+    if title != '':
+        Notes.query.filter_by(user_id=curr_user_id, id=note_id).update(
+            {'title': title, 'detail_text': details, 'date_upd': datetime.utcnow()})
+        db.session.flush()
+        db.session.commit()
+    return redirect(url_for('note_detail', note_id=note_id))
+
